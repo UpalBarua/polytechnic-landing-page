d433ebe4c2f87990c44f3960334d3f54a835e7b2
UpalBarua <upalbarua52@gmail.com>
2024-01-14 14:35:22 +0600

    created chairmans message

---
+++ src/components/chairmans-message.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import { FaQuoteRight } from 'react-icons/fa';
+
+const chairmansInfo = {
+  picture: '/chairman.jpg',
+  message:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, nulla sed alias laboriosam repellendus sint eius aliquam suscipit ad optio a sequi vero, et tenetur aperiam temporibus odio repudiandae? Fugit aliquid explicabo incidunt, voluptatibus, nemo cupiditate maxime eaque perspiciatis debitis, nulla corporis excepturi autem. Quas nisi quae optio aperiam eos?',
+};
+
+export function ChairmansMessage() {
+  return (
+    <div className="mx-auto space-y-4 max-w-sm rounded-md sm:border sm:shadow-sm sm:max-w-full sm:p-6">
+      <Image
+        className="object-cover object-center rounded-full border aspect-square"
+        src={chairmansInfo.picture}
+        alt="chairman"
+        height={140}
+        width={140}
+        quality={90}
+      />
+      <div className="flex gap-x-6 items-center">
+        <h2 className="text-2xl font-medium tracking-tight">
+          Message From Chairman
+        </h2>
+        <FaQuoteRight className="text-4xl text-foreground/20" />
+      </div>
+      <p className="leading-relaxed text-foreground/80">
+        {chairmansInfo.message}
+      </p>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
+import { ChairmansMessage } from '@/components/chairmans-message';
+
 function HomePage() {
   return (
-    <main className="grid grid-cols-12 gap-4">
+    <main className="grid grid-cols-12 gap-8 md:gap-4">
       <section className="col-span-full p-5 bg-red-200 md:col-span-7">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus fugiat
         accusantium illum recusandae eaque, asperiores et odio fugit earum
         autem.
       </section>
-      <section className="col-span-full p-5 bg-green-200 md:col-span-5">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis vero
-        ipsam laudantium molestiae ipsa asperiores saepe dolor voluptas nobis
-        cumque!
+      <section className="col-span-full md:col-span-5">
+        <ChairmansMessage />
       </section>
     </main>
   );
