c35406affa05cbdf58487f4cb2d67ff72bfc7460
UpalBarua <upalbarua52@gmail.com>
2024-01-14 20:58:17 +0600

    refactored ui

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,41 +1,12 @@
 import {
   Carousel,
   CarouselContent,
-  type CarouselApi,
   CarouselItem,
-} from "@/components/ui/carousel";
+  type CarouselApi,
+} from '@/components/ui/carousel';
+import Image from 'next/image';
+import React from 'react';
 
-export function Example() {
-  const [api, setApi] = React.useState<CarouselApi>();
-  const [current, setCurrent] = React.useState(0);
-  const [count, setCount] = React.useState(0);
-
-  React.useEffect(() => {
-    if (!api) {
-      return;
-    }
-
-    setCount(api.scrollSnapList().length);
-    setCurrent(api.selectedScrollSnap() + 1);
-
-    api.on("select", () => {
-      setCurrent(api.selectedScrollSnap() + 1);
-    });
-  }, [api]);
-
-  return (
-    <Carousel setApi={setApi}>
-      <CarouselContent>
-        <CarouselItem>...</CarouselItem>
-        <CarouselItem>...</CarouselItem>
-        <CarouselItem>...</CarouselItem>
-      </CarouselContent>
-    </Carousel>
-  );
-}
-
-import Image from "next/image";
-import React from "react";
 const Hero = () => {
   const [api, setApi] = React.useState<CarouselApi>();
   const [current, setCurrent] = React.useState(0);
@@ -49,7 +20,7 @@ const Hero = () => {
     setCount(api.scrollSnapList().length);
     setCurrent(api.selectedScrollSnap() + 1);
 
-    api.on("select", () => {
+    api.on('select', () => {
       setCurrent(api.selectedScrollSnap() + 1);
     });
   }, [api]);
@@ -58,31 +29,31 @@ const Hero = () => {
     <Carousel setApi={setApi}>
       <CarouselContent>
         <CarouselItem>
-          {" "}
           <Image
             src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZWR1Y2F0aW9ufGVufDB8fDB8fHww"
             alt=""
             height={700}
             width={800}
-          ></Image>
+            quality={95}
+            className="object-center object-cover rounded-md shadow-sm"></Image>
         </CarouselItem>
         <CarouselItem>
-          {" "}
           <Image
             src="https://images.unsplash.com/photo-1613592498887-da347580ec0a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTF8fGNhbXB1c3xlbnwwfHwwfHx8MA%3D%3D"
             alt=""
             height={700}
             width={800}
-          ></Image>
+            quality={95}
+            className="object-center object-cover rounded-md shadow-sm"></Image>
         </CarouselItem>
         <CarouselItem>
-          {" "}
           <Image
             src="https://images.unsplash.com/photo-1569447891824-7a1758aa73a2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDB8fGNhbXB1c3xlbnwwfHwwfHx8MA%3D%3D"
             alt=""
             height={700}
             width={800}
-          ></Image>
+            quality={95}
+            className="object-center object-cover rounded-md shadow-sm"></Image>
         </CarouselItem>
       </CarouselContent>
     </Carousel>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import Hero from '@/components/Hero';
 import { ChairmansMessage } from '@/components/chairmans-message';
+import { Departments } from '@/components/departments';
 import { LatestUpdate } from '@/components/latest-update';
 import { Noticeboard } from '@/components/noticeboard';
 import { UpcomingEvents } from '@/components/upcoming-events';
@@ -6,10 +8,9 @@ import { UpcomingEvents } from '@/components/upcoming-events';
 function HomePage() {
   return (
     <main className="grid grid-cols-12 gap-8 md:gap-2">
-      <section className="col-span-full p-5 bg-red-200 md:col-span-7">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus fugiat
-        accusantium illum recusandae eaque, asperiores et odio fugit earum
-        autem.
+      <section className="col-span-full md:col-span-7">
+        <Hero />
+        <Departments />
       </section>
       <section className="col-span-full space-y-8 lg:space-y-2 md:col-span-5">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-2">
