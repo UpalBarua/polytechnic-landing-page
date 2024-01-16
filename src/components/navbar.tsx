2045bb859442be5736a98ff2fac1f0f1f2791161
UpalBarua <upalbarua52@gmail.com>
2024-01-16 13:39:21 +0600

    added new primary color and fixed navbar bug

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,16 +10,15 @@ import { navLinks } from '@/config';
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { FaChevronRight, FaSchool } from 'react-icons/fa';
+import { FaChevronDown, FaChevronRight, FaSchool } from 'react-icons/fa';
 import MobileNav from './mobile-nav';
-import { FaChevronDown } from 'react-icons/fa';
 
 export function Navbar() {
   return (
-    <header className="border-b py-5 fixed w-full left-0 top-0 bg-white shadow-sm z-10">
+    <header className="border-b py-4 fixed w-full left-0 top-0 bg-white shadow z-10">
       <div className="container flex gap-y-4 flex-col items-center justify-center max-w-6xl">
         <Link href="/" className="flex items-center mr-8 space-x-3">
-          <FaSchool className="text-4xl" />
+          <FaSchool className="text-4xl text-primary" />
           <span className="hidden text-2xl font-bold capitalize sm:inline-block">
             সামশুন নাহার হারুন পলিটেকনিক ইনস্টিটিউট
           </span>
@@ -39,7 +38,7 @@ function NavItem({ route, label, subRoutes }: NavLink) {
   const pathname = usePathname();
 
   return subRoutes ? (
-    <DropdownMenu key={route}>
+    <DropdownMenu key={route} modal={false}>
       <DropdownMenuTrigger asChild>
         <Button
           variant="link"
@@ -51,12 +50,28 @@ function NavItem({ route, label, subRoutes }: NavLink) {
               : 'text-foreground/60'
           )}>
           <span>{label}</span>
-          <FaChevronDown className="text-xs" />
+          <FaChevronDown className="text-xs text-foreground/40" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         {subRoutes.map(({ route, label }) => (
-          <DropdownMenuItem key={route}>{label}</DropdownMenuItem>
+          <DropdownMenuItem key={route}>
+            <Button
+              variant="link"
+              size="sm"
+              className={cn(
+                'text-base hover:text-foreground/80',
+                pathname?.startsWith('/docs/components')
+                  ? 'text-foreground'
+                  : 'text-foreground/60'
+              )}
+              asChild>
+              <Link href={route}>
+                <span>{label}</span>
+                <FaChevronRight className="text-xs text-foreground/40" />
+              </Link>
+            </Button>
+          </DropdownMenuItem>
         ))}
       </DropdownMenuContent>
     </DropdownMenu>
@@ -73,7 +88,7 @@ function NavItem({ route, label, subRoutes }: NavLink) {
       asChild>
       <Link href={route}>
         <span>{label}</span>
-        <FaChevronRight className="text-xs" />
+        <FaChevronRight className="text-xs text-foreground/40" />
       </Link>
     </Button>
   );
