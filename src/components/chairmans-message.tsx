cb26e674a27bfda86ffd4baa9b1c8882066fe4d9
UpalBarua <upalbarua52@gmail.com>
2024-01-16 14:32:23 +0600

    added chairman page

--- src/components/chairmans-message.tsx
+++ src/components/chairmans-message.tsx
@@ -1,31 +1,32 @@
 import Image from 'next/image';
 import { FaQuoteRight } from 'react-icons/fa';
-
-const chairmansInfo = {
-  picture: '/chairman.jpg',
-  message:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, nulla sed alias laboriosam repellendus sint eius aliquam suscipit ad optio a sequi vero, et tenetur aperiam temporibus odio repudiandae? Fugit aliquid explicabo incidunt, voluptatibus, nemo cupiditate maxime eaque perspiciatis debitis, nulla corporis excepturi autem. Quas nisi quae optio aperiam eos?',
-};
+import Link from 'next/link';
+import { chairmansMessage } from '@/config';
 
 export function ChairmansMessage() {
   return (
-    <div className="mx-auto space-y-4 max-w-sm rounded-md sm:border sm:shadow-sm sm:max-w-full sm:p-6">
+    <div className="mx-auto space-y-3 max-w-sm rounded-md sm:border sm:shadow-sm sm:max-w-full sm:p-6">
       <Image
         className="object-cover object-center rounded-full border aspect-square"
-        src={chairmansInfo.picture}
+        src={chairmansMessage.picture}
         alt="chairman"
-        height={140}
-        width={140}
+        height={150}
+        width={150}
         quality={90}
       />
       <div className="flex gap-x-6 items-end">
-        <h2 className="text-2xl font-medium tracking-tight">
-          Message From Chairman
+        <h2 className="text-2xl font-bold tracking-tight">
+          চেয়ারম্যানের বাণী
         </h2>
-        <FaQuoteRight className="text-4xl text-foreground/20" />
+        <FaQuoteRight className="text-5xl text-primary/25" />
       </div>
       <p className="leading-relaxed text-foreground/80">
-        {chairmansInfo.message}
+        {chairmansMessage.message.slice(0, 500) + '...'}
+        <Link
+          href="/chairmans-message"
+          className="font-bold underline underline-offset-2">
+          আরো পড়ুন
+        </Link>
       </p>
     </div>
   );
---
+++ src/pages/chairman.tsx
@@ -0,0 +1,26 @@
+import { chairmansMessage } from '@/config';
+import Image from 'next/image';
+
+function Chairman() {
+  return (
+    <main className="container space-y-4 max-w-3xl pb-28">
+      <Image
+        className="object-cover object-center shadow rounded-full border aspect-square"
+        src={chairmansMessage.picture}
+        alt="chairman"
+        height={180}
+        width={180}
+        quality={90}
+      />
+      <div>
+        <h3 className="text-2xl font-bold">{chairmansMessage.name}</h3>
+        <span className="text-lg font-medium">{chairmansMessage.position}</span>
+      </div>
+      <p className="leading-relaxed text-foreground/80">
+        {chairmansMessage.message}
+      </p>
+    </main>
+  );
+}
+
+export default Chairman;
