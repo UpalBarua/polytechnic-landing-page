9217937e7a0241e2285029adda486a40f6714434
UpalBarua <upalbarua52@gmail.com>
2024-01-17 16:47:55 +0600

    ui: refactor home page slider

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -16,13 +16,15 @@ export function Navbar() {
     <header className="fixed top-0 left-0 z-10 w-full border-b shadow bg-primary text-background">
       <div className="container flex py-4 max-w-7xl">
         <Link href="/" className="flex items-center space-x-3">
-          <FaSchool className="text-3xl" />
+          <div className="flex justify-center items-center w-8 rounded-full shadow aspect-square bg-background/60 text-background">
+            <FaSchool className="text-xl" />
+          </div>
           <span className="hidden text-xl font-bold capitalize text-foreground sm:inline-block">
             সামশুন নাহার হারুন পলিটেকনিক ইনস্টিটিউট
           </span>
         </Link>
       </div>
-      <div className="flex justify-center items-center py-2 bg-background/60">
+      <div className="flex justify-center items-center py-2 bg-background/100">
         <nav className="hidden gap-x-2 items-center text-sm md:flex">
           {navLinks.map((link) => (
             <NavItem key={link.route} {...link} />
