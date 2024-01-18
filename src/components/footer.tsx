9ab8da96e2101bfd9ecebed7ab28a2b3dc88a731
niaz abir <niazabir55@gmail.com>
2024-01-18 16:22:05 +0600

    gallery commit

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,15 +4,20 @@ import React from "react";
 import { HiPhoneIncoming } from "react-icons/hi";
 import { MdAttachEmail } from "react-icons/md";
 import { SiFacebook } from "react-icons/si";
+import { FaSchool } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <section className="mt-14">
-      <div className="flex gap-24">
-        <div>
-          <h1 className="text-2xl font-bold">সামশুন নাহার হারুন </h1>
-          <h1 className="text-2xl font-bold">পলিটেকনিক ইনস্টিটিউট</h1>
-          <hr className="mt-6"></hr>
+    <section className="mt-14 mb-8 pt-4 pb-2 bg-orange-200 ">
+      <div className="flex gap-14 pl-4 lg:pl-14">
+        <div className="pl-14 pb-8 flex-1">
+          <div className="flex gap-4 items-center">
+            <FaSchool className="text-2xl font-bold "> </FaSchool>
+            <h1 className="text-2xl font-bold ">
+              সামশুন নাহার হারুন <br></br>পলিটেকনিক ইনস্টিটিউট{" "}
+            </h1>
+          </div>
+          <hr className="mt-6 "></hr>
           <h1 className="pt-4 text-[20px] font-semibold">
             Muradpur,Chittagong
           </h1>
@@ -30,8 +35,13 @@ const Footer = () => {
             <h1>Samsur Nahar Harun</h1>
           </div>
         </div>
-        <div>
-          <ReactMap></ReactMap>
+        <div className="relative w-full lg:w-[700px] p ">
+          <Image
+            src="https://images.unsplash.com/photo-1486976862325-fbac7b41739b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+            className="object-cover object-center pr-2 rounded-md lg:pr-6 "
+            fill
+          ></Image>
         </div>
       </div>
     </section>
